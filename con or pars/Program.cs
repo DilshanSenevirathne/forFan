8d6adb713a0872970a050d76855e3e1a5f19af15
jiggasl <kdsenevirathne@gmail.com>
2021-02-12 11:25:56 +0530

    Add project files.

---
+++ con or pars/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace con_or_pars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A part
+            for (int i = 1; i <= 9; i++)
+            {
+               
+                for (int x = 0; i > x; x++)
+                {
+                    if (x == 1 & i == 3)
+                    {
+                        Console.Write("  ");
+                        continue;                        
+                    }
+                    if (x == 1 & i == 4)
+                    {
+                        Console.Write("  ");
+                        continue;
+                    }
+
+
+
+                    Console.Write(x+ " ");                  
+                }
+                for (int x = i; x <9; x++)
+                {
+                    Console.Write("  ");                  
+                }
+                for (int a = i; a <= 9; a++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine("");              
+            }
+            //C part
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int z = 9; i < z; z--)
+                {
+                    Console.Write("  ");
+                }
+                for (int x = 0; i > x; x++)
+                {
+                    Console.Write("*" + " ");
+                }
+                for (int p = 1; p<i; p++)
+                {
+                    Console.Write("  ");
+                }
+                for (int y = i; y <=9 ; y++)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine("");
+            }            
+        }
+    }
+}
---
+++ con or pars/person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace con_or_pars
+{
+    class person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public string fullName 
+            
+        {
+            set
+            {
+                firstName = "S";
+            }
+            get
+            {
+                return firstName + " " + lastName;
+            }
+
+          
+        
+        }
+
+        public string fullName1()
+        {
+           return firstName +" "+ lastName;
+        }
+
+    }
+}
